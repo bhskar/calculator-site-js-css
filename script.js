abdbe1720f8ace3c54b0f972f3e0ae752ce5fd24
Bhaas-kar <110971795+bhskar@users.noreply.github.com>
2023-05-17 23:58:58 +0530

    added calculate() in script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-// This function display values
 function display(value) {
   document.getElementById("result").value += value;
 }
 
-// This function display values
 function display(value) {
   document.getElementById("result").value += value;
 }
 
+function calculate() {
+  var p = document.getElementById("result").value;
+  var q = eval(p);
+  document.getElementById("result").value = q;
+}
