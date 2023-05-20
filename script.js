be66085db1aa6a95efe6a35bb7c660a0389b1462
Bhaas-kar <110971795+bhskar@users.noreply.github.com>
2023-05-20 15:05:15 +0530

    rewrote calculate() in /script.js to avoid use of eval() in it

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ function display(value) {
   document.getElementById("result").value += value;
 }
 
-// This function evaluates the expression and returns result
+//This function evaluates the expression and returns result
+// function calculate() {
+//   let a = document.getElementById("result").value;
+//   let b = eval(a);
+//   document.getElementById("result").value = b;
+// }
+
 function calculate() {
-  let a = document.getElementById("result").value;
-  let b = eval(a);
-  document.getElementById("result").value = b;
+  const inputElement = document.getElementById("result");
+  const expression = inputElement.value;
+  const result = Function(`return ${expression}`) ();
+  inputElement.value = result;
 }
+
